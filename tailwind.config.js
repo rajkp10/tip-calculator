98f07a53af60f7c8f968ae3048dd45f8e60b0424
Raj <patelraj10401@gmail.com>
2022-09-20 20:21:22 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        StrongCyan: "hsl(172, 67%, 45%)",
+        VeryDarkCyan: "hsl(183, 100%, 15%)",
+        DarkGrayishCyan: "hsl(186, 14%, 43%)",
+        GrayishCyan: "hsl(184, 14%, 56%)",
+        LightGrayishCyan: "hsl(185, 41%, 84%)",
+        VeryLightGrayishCyan: "hsl(189, 41%, 97%)",
+        White: "hsl(0, 0%, 100%)",
+      },
+    },
+  },
+  plugins: [],
+};
